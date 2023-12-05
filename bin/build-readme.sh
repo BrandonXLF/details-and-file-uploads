f8fa96e72d6c07e568a4243ca3d2765a8a88bf7f
Brandon Fowler <brandfowl@gmail.com>
2023-12-05 01:18:30 -0500

    generate-readme --> build-readme

--- bin/build-readme.sh
+++ bin/build-readme.sh
@@ -3,4 +3,4 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 python3 -m pip install pyyaml
-python3 $SCRIPT_DIR/generate-readme.py
+python3 $SCRIPT_DIR/build-readme.py
