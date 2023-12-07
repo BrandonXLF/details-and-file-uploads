ad8bf20b692e0f83acc95171105fd88ca675bc73
Brandon Fowler <brandfowl@gmail.com>
2023-12-06 19:23:45 -0500

    Use dirname to get top directory

--- bin/build-zip.sh
+++ bin/build-zip.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-VERSION=$( grep -oP "^version: '\K[^']+" $SCRIPT_DIR/../readme-template.yml )
+TOP_DIR=$( dirname -- SCRIPT_DIR )
+VERSION=$( grep -oP "^version: '\K[^']+" $TOP_DIR/readme-template.yml )
 
-cd $SCRIPT_DIR/..
-git archive --format zip --output $SCRIPT_DIR/../details-and-file-upload.$VERSION.zip --prefix details-and-file-upload/ HEAD
+cd $TOP_DIR
+git archive --format zip --output $TOP_DIR/details-and-file-upload.$VERSION.zip --prefix details-and-file-upload/ HEAD
